7a8781df08804d8c4bb01635bd2ada7dd8864822
Daniel Vargas <Davargas@espol.edu.ec>
2021-10-06 15:50:45 -0500

    Primer commit de la materia

---
+++ hola.c
@@ -0,0 +1,4 @@
+Buenos dias profesor, este es mi primer commit, hasta ahora entiendo los conmandos basicos que nos 
+dio en clases, espero poder entender mejor la materia, y aprender mas sobre los servicios
+que ofrece Linux y los sistemas operativos basados en estos, tambien espero 
+aprender mas sobre los clientes y servidores dentro de una red
